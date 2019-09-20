b1e2ec6a7f18ea43384896bb57ff3ae5e90ac608
Adam-Xi <321041835@qq.com>
2019-09-20 23:26:20 +0800

    矩阵中两坐标相距最短路径（修改）

--- ShortestPathBetweenMN.cpp
+++ ShortestPathBetweenMN.cpp
@@ -27,29 +27,30 @@ X星球居民小区的楼房全是一样的，并且按矩阵样式排列。其楼房的编号为1,2,3...
 #include<cmath>
 using namespace std;
 
+
 int OKrow(int w, int num)  //确定行号
 {
-	return num % w ? (num / w - 1) : (num / w);
+	return num % w == 0 ? (num / w) : (num / w + 1);
 }
 int  OKcol(int w, int num)  //确定列号
 {
-	if ((num & 1) == 1) {//第偶数行,顺着走
-
-		return (w - num % w) % w;
-
+	if ((OKrow(w, num) & 1) == 1) //第奇数行,顺着走
+	{
+		return ((num % w - 1 + w) % w + 1);
 	}
-	else {//奇数行
-
-		return (num % w - 1 + w) % w;
-
+	else //偶数行
+	{
+		return ((w - num % w + 1) % w);
 	}
 }
 int ShortestPath(int w, int m, int n)
 {
 	int posR1 = OKrow(w, m);
-	int posC1 = OKrow(w, m);
+	int posC1 = OKcol(w, m);
 	int posR2 = OKrow(w, n);
 	int posC2 = OKcol(w, n);
+	cout << posR1 << ", " << posC1 << endl;
+	cout << posR2 << ", " << posC2 << endl;
 
 	return abs(posR1 - posR2) + abs(posC1 - posC2);
 }
